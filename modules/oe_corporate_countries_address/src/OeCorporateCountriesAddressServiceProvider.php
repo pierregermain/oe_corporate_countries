6fe7436097568a550188bdcf7dc82d92d516b7cd
Francesco Sardara <francesco.sardara@ausy.be>
2020-04-15 18:26:33 +0200

    OPENEUROPA-2288: Override the country repository service to load country definitions from Publication Office vocabulary.

---
+++ modules/oe_corporate_countries_address/src/OeCorporateCountriesAddressServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_corporate_countries_address;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\DependencyInjection\ServiceModifierInterface;
+use Drupal\oe_corporate_countries_address\Repository\CountryRepository;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Replaces the address.country_repository service with our implementation.
+ */
+class OeCorporateCountriesAddressServiceProvider implements ServiceModifierInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alter(ContainerBuilder $container) {
+    if ($container->has('address.country_repository')) {
+      $container->getDefinition('address.country_repository')
+        ->setClass(CountryRepository::class)
+        ->setArguments([
+          new Reference('cache.data'),
+          new Reference('language_manager'),
+          new Reference('entity_type.manager'),
+          new Reference('entity.repository'),
+          new Reference('sparql_endpoint'),
+        ]);
+    }
+  }
+
+}
