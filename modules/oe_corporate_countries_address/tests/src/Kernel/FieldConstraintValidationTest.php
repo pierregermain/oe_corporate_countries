be5b396f73b1a48e6c0bdd3bf635c626bb487242
Francesco Sardara <francesco.sardara@ausy.be>
2020-04-22 17:37:04 +0200

    OPENEUROPA-2288: Test the constraint on address field type.

--- modules/oe_corporate_countries_address/tests/src/Kernel/FieldConstraintValidationTest.php
+++ modules/oe_corporate_countries_address/tests/src/Kernel/FieldConstraintValidationTest.php
@@ -38,21 +38,24 @@ class FieldConstraintValidationTest extends CorporateCountriesRdfKernelTestBase 
   }
 
   /**
-   * Tests that constraints are applied to the country field type.
+   * Tests that the deprecation constraint is applied to the country field type.
    */
-  public function testCountryFieldType(): void {
+  public function testCountryFieldTypeDeprecationConstraint(): void {
     $this->createField('address_country');
 
     /** @var \Drupal\entity_test\Entity\EntityTest $entity */
     $entity = EntityTest::create();
+    // When no country value is present, there are no validation errors.
     $this->assertCount(0, $entity->validate());
 
+    // Setting an invalid country will trigger a validation error.
     $entity->set('field_test', 'XY');
     $violations = $entity->validate();
     $this->assertCount(1, $violations);
     $this->assertEquals('The country <em class="placeholder">&quot;XY&quot;</em> is not valid.', $violations[0]->getMessage());
     $this->assertEquals('field_test.0.value', $violations[0]->getPropertyPath());
 
+    // Setting a deprecated country will trigger a deprecation error.
     $entity->set('field_test', 'AN');
     $violations = $entity->validate();
     $this->assertCount(2, $violations);
@@ -60,6 +63,62 @@ class FieldConstraintValidationTest extends CorporateCountriesRdfKernelTestBase 
     $this->assertEquals('field_test.0.value', $violations[0]->getPropertyPath());
     $this->assertEquals('The country <em class="placeholder">&quot;AN&quot;</em> is not available.', $violations[1]->getMessage());
     $this->assertEquals('field_test.0.value', $violations[0]->getPropertyPath());
+
+    // Valid country values do not trigger any error.
+    $entity->set('field_test', 'IT');
+    $this->assertCount(0, $entity->validate());
+  }
+
+  /**
+   * Tests that the deprecation constraint is applied to the address field type.
+   */
+  public function testAddressFieldTypeDeprecationConstraint(): void {
+    $this->createField('address');
+
+    /** @var \Drupal\entity_test\Entity\EntityTest $entity */
+    $entity = EntityTest::create();
+    // When no country value is present, there are no validation errors.
+    $this->assertCount(0, $entity->validate());
+
+    // Setting an invalid country will trigger a validation error.
+    $entity->set('field_test', [
+      'country_code' => 'XY',
+      'address_line1' => 'Via Sardegna',
+      'locality' => 'Roma',
+      'given_name' => 'Mario',
+      'family_name' => 'Rossi',
+    ]);
+    $violations = $entity->validate();
+    $this->assertCount(1, $violations);
+    $this->assertEquals('The country <em class="placeholder">&quot;XY&quot;</em> is not valid.', $violations[0]->getMessage());
+    $this->assertEquals('field_test.0.country_code', $violations[0]->getPropertyPath());
+
+    // Setting a deprecated country will trigger a deprecation error.
+    $entity->set('field_test', [
+      'country_code' => 'AN',
+      'address_line1' => 'Via Sardegna',
+      'locality' => 'Roma',
+      'given_name' => 'Mario',
+      'family_name' => 'Rossi',
+    ]);
+    $violations = $entity->validate();
+    $this->assertCount(2, $violations);
+    $this->assertEquals('The country "<em class="placeholder">AN</em>" is deprecated. Please specify a replacement.', $violations[0]->getMessage());
+    $this->assertEquals('field_test.0.country_code', $violations[0]->getPropertyPath());
+    $this->assertEquals('The country <em class="placeholder">&quot;AN&quot;</em> is not available.', $violations[1]->getMessage());
+    $this->assertEquals('field_test.0.country_code', $violations[0]->getPropertyPath());
+
+    // Valid country values do not trigger any error.
+    $entity->set('field_test', [
+      'country_code' => 'IT',
+      'address_line1' => 'Via Sardegna',
+      'locality' => 'Roma',
+      'administrative_area' => 'RM',
+      'postal_code' => '00100',
+      'given_name' => 'Mario',
+      'family_name' => 'Rossi',
+    ]);
+    $this->assertCount(0, $entity->validate());
   }
 
   /**
